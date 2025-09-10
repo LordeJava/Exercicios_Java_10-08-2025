d6ea1514ca4e9e31f7d970b494e93c0733ee3c41
Heitor Barbosa Fernandes de Jesus <heitorjessica08@gmail.com>
2025-09-10 11:27:33 -0300

    forAninhado.java

--- logica/forAninhado.java
+++ logica/forAninhado.java
@@ -1,5 +1,13 @@
 package logica;
 
 public class forAninhado {
-    
-}
+    public static void main(String[] args) {
+            for (int i = 1; i <= 3; i++) {
+                for (int j = 1; j <= 3; j++) {
+                    System.out.printf("%d x %d = %d ", i, j, i*j);
+                }
+                System.out.println();
+            }
+        }
+
+    }
