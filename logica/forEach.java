c19bbb86b9b0d480b36ddb59e4bbef2436c40fa4
Heitor Barbosa Fernandes de Jesus <heitorjessica08@gmail.com>
2025-09-10 11:28:00 -0300

    forEach.java

--- logica/forEach.java
+++ logica/forEach.java
@@ -1,3 +1,12 @@
 package logica;
 
-public class forEach{}
+public class forEach{
+    public class ForEachExemplo {
+        public static void main(String[] args) {
+            int[] numeros = {10, 20, 30};
+            for (int n : numeros) {
+                System.out.println("Número: " + n);
+            }
+        }
+    }
+}
