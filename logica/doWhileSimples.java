b3ab4ccd1480f895b213e81c262706d810d41de5
Heitor Barbosa Fernandes de Jesus <heitorjessica08@gmail.com>
2025-09-10 11:26:46 -0300

    doWhileSimples.java

--- logica/doWhileSimples.java
+++ logica/doWhileSimples.java
@@ -1,8 +1,15 @@
 package logica;
 
 import java.util.Scanner;
-
 public class  doWhileSimples{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        do {
+            System.out.print("Digite um número (0 para sair): ");
+            num = sc.nextInt();
+        } while (num != 0);
+        sc.close();
+    }
 
 }
-
